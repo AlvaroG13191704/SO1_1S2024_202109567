38f11c174c7b4cfb1abe15054ab33e9da2c561dd
Alvaro Garcia <3046971840116@ingenieria.usac.edu.gt>
2024-03-15 21:11:31 -0600

    adding reverse proxy

--- Proyecto1/frontend/next.config.mjs
+++ Proyecto1/frontend/next.config.mjs
@@ -1,14 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://192.168.175.128:8080/api/:path*',
-      },
-    ];
-  },
   output: "standalone",
 };
 
 export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: 'http://192.168.175.128:8080/api/:path*',
+//       },
+//     ];
+//   },
+//   output: "standalone",
+// };
+
+// export default nextConfig;
