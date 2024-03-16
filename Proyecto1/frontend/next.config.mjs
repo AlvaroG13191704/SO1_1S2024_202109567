38fb53933185cf879aa54cfc69197accc2406962
Alvaro Garcia <3046971840116@ingenieria.usac.edu.gt>
2024-03-15 20:18:21 -0600

    editing backend and frontend

--- Proyecto1/frontend/next.config.mjs
+++ Proyecto1/frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://192.168.175.128:8080/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
       },
     ];
   },
